f1af37576a64509729ba7804252d04292ed515f5
Lahkust <Guillaume.Robert.Bergs@gmail.com>
2014-09-21 15:50:49 -0400

    MàJ dictionnaire
    Codage de la lettre présente, actualiser lettres, mot aléatoire
    Suppression de la redéfinition du destructeur par défaut.

--- dictionnaire.cpp
+++ dictionnaire.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <random>
 #include <string>
 
 #include "dictionnaire.h"
@@ -11,10 +12,6 @@ dictionnaire::dictionnaire()
   map_associative = map< mot, set < mot > >();
 }
 
-dictionnaire::~dictionnaire()
-{
-  //dtor
-}
 
 void dictionnaire::ouvrir(string ods6, uint32_t taille_mot)
 {
@@ -35,11 +32,35 @@ void dictionnaire::ouvrir(string ods6, uint32_t taille_mot)
 	}
 	else cout << "Unable to open file";
 
-
-
 	cout << map_dictionnaire.find(uint32_t(0))->second.value;
 
 
 }
 
+void dictionnaire::actualiser_lettres(char lettre)
+{
+  utilisees += lettre;
+  if(lettre>90)lettre -= 32;        //Si la lettre est minuscule, la convertir en majuscule
+  disponibles.at(lettre - 41)='-';  //Ex: pour C, passer de "ABCDEFG..." à "AB-DEFG..."
+}
 
+bool dictionnaire::lettre_presente(char lettre)
+{
+bool retour = false;
+mot test = map_dictionnaire.find(indice_mot_cible)->second;
+//for(size_t i = 0; i < test.size(); ++i)retour |= (test.at(i) == lettre);
+return retour;
+}
+
+mot dictionnaire::mot_aleatoire()
+{
+  mt19937 rng;          // Crée un générateur de nombre aléatoires (standard C++11)
+  rng.seed(uint32_t()); // Initialisation à l'aide d'une graine elle-même aléatoire (la scrap en mémoire)
+  uniform_int_distribution<uint32_t> uint_dist(0,map_dictionnaire.size()); //Définit les limites du rng
+  return map_dictionnaire.find(uint_dist(rng))->second; //Génère concrètement l'index aléatoire et retourne le mot associé
+}
+
+void dictionnaire::comparer(char)
+{
+
+}
--- dictionnaire.h
+++ dictionnaire.h
@@ -10,23 +10,17 @@ class dictionnaire
 {
 public:
 
-
     void ouvrir(string,uint32_t);  //Ouvrir le fichier indiqué par le string et récupérer les mots de longueur uint32_t
 
     dictionnaire();
-    ~dictionnaire();
 
     void actualiser_lettres(char); // Retire la lettre des lettres disponibles et ajoute la lettre aux lettrres utilisées
     bool lettre_presente(char);    // Vérifie si le char est présent dans le mot cible
-
-
-
 
 private:
 
     map< uint32_t, mot > map_dictionnaire;      // ensemble des mots tirés du fichier ODS6
     map< mot, set < mot > > map_associative;    // ensemble des mots utilisés lors de la triche
-
 
     string disponibles;         //lettres disponibles
     string utilisees;           //lettres utilisées
@@ -34,7 +28,7 @@ private:
     uint32_t indice_mot_cible;
     mot mot_aleatoire();        // Génère un indice aléatoire et en retourne le mot associé
     void comparer(char);        // Compare le char avec le mot cible, vérifie si la lettre est
-    // présente, met à jour l'affichage des lettres, etc
+                                // présente, met à jour l'affichage des lettres, etc
 
 };
 
